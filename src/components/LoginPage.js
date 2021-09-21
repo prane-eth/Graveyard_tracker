889c952944aa3071bec9ba4b3ef3a363a24025b9
Praneeth <vhpraneeth2001@gmail.com>
2021-09-21 18:14:33 +0530

    Edited files

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -11,12 +11,6 @@ export class LoginPage extends React.Component {
         var password = document.getElementById('password')
         email = email.value
         password = password.value
-        if (email == "dummy@gmail.com") {
-            var accessToken = '918245y1824'
-            cookie.save('accessToken', accessToken, { path: '/' })
-            window.location.href = "/addData"  // redirect to same page to login
-            return accessToken
-        }
         let url = backendURL + '/login?email=' + email + '&password=' + password
         let res = await axios.get(url)
         if ('error' in res)
@@ -26,8 +20,7 @@ export class LoginPage extends React.Component {
             cookie.save('accessToken', accessToken, { path: '/' })
             window.location.href = "/addData"
             return accessToken
-        } else
-            return ''
+        }
     }
     signup = async () => {
         var email = document.getElementById('email')
@@ -49,18 +42,41 @@ export class LoginPage extends React.Component {
         let accessToken = cookie.load('accessToken')
         if (accessToken)
             return <Redirect to="/addData" />
-        return (<div className="loginPage">
-            <input type="email" defaultValue="dummy@gmail.com" placeholder="Email" id="email"/>
-            <br />
-            <input type="password" defaultValue="dummy123" placeholder="Password" id="password"/>
-            <br />
-            <input type="button" onClick={() => {this.login()}} value="Login"/>
-            <input type="button" onClick={() => {this.signup()}} value="Signup"/>
-        </div>)
+        return (
+            <div className="loginPage">
+                <input type="email" defaultValue="dummy@gmail.com" placeholder="Email" id="email"/>
+                <br />
+                <input type="password" defaultValue="dummy123" placeholder="Password" id="password"/>
+                <br />
+                <input type="button" onClick={() => {this.login()}} value="Login"/>
+                <input type="button" onClick={() => {this.signup()}} value="Signup"/>
+            </div>
+        )
     }
 }
 
 export class AddDataPage extends React.Component {
+    submitValues = () => {
+        var name = document.getElementById('name').value
+        var pinCode = document.getElementById('pinCode').value
+        var occupied = document.getElementById('occupied').value
+        var vacancies = document.getElementById('vacancies').value
+        var address = document.getElementById('address').value
+        var msgDiv = document.getElementById('errorMsg')
+        if (!(name && pinCode && occupied && vacancies && address)) {  // any empty value
+            msgDiv.style = 'color: red'
+            msgDiv.innerText = 'Please enter all the values'
+            return
+        }
+        if (""+pinCode.length != 6)  {
+            msgDiv.style = 'color: lightred'
+            msgDiv.innerText = 'Pin Code should have only 6 digits'
+            return
+        }
+        console.log(name, pinCode, occupied, vacancies, address)
+        msgDiv.style = 'color: green'
+        msgDiv.innerText = 'Updated data successfully. Thank you.'
+    }
     render()    {
         let accessToken = cookie.load('accessToken')
         if (!accessToken)
@@ -72,7 +88,11 @@ export class AddDataPage extends React.Component {
             Occupied:  <input type="number" placeholder="Occupied" id="occupied"/> <br />
             Vacancies:  <input type="number" placeholder="Vacancies" id="vacancies"/> <br />
             Address:  <input type="text" placeholder="Address" id="address"/> <br />
-            <input type="button" id="button" value="Submit" className="submitButton"/>
+            <input type="button" id="button" value="Submit" className="submitButton" 
+                onClick={() => {this.submitValues()}}/> <br />
+            <input type="button" id="button" value="Logout" className="submitButton" 
+                onClick={() => { window.location.href = '/logout'}} /> <br />
+            <p className="errorMsgClass" id="errorMsg"></p>
         </div>)
     }
 }
